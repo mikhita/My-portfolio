48975918e0ce5c8f92f6243723345e5779296f09
mikhita <mikheilispiriani@yahoo.com>
2022-08-07 18:39:06 +0400

    I finally finish my slider task with using regex and transformation of numbers into negative using the question symbol, and I am very excited for that because my skills are more and more better every day

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 let arrowLeft = document.getElementsByClassName("arrowLeft")[0];
 let arrowRight = document.getElementsByClassName("arrowRight")[0];
 let sliderMainDiv = document.querySelector(".sliderMainDiv");
-let click = 0;
-let clickRight = 0;
+let pattern = /-?\d+/gm;
 
 arrowLeft.addEventListener('click', function(){
-    sliderMainDiv.style.transform = "translate3d(21%,0,0)"
+    let currentTranslateX = parseInt(sliderMainDiv.getAttribute("style").match(pattern)[1]);
+    if(currentTranslateX===42){
+        sliderMainDiv.style.transform = `translate3d(0,0,0)`
+    }else{
+        sliderMainDiv.style.transform = `translate3d(${currentTranslateX+21}%,0,0)`
     
+    }
 });
 
 arrowRight.addEventListener('click', function(){
-    sliderMainDiv.style.transform = "translate3d(0,0,0)"
+    let currentTranslateX = parseInt(sliderMainDiv.getAttribute("style").match(pattern)[1]);
+    console.log(currentTranslateX);
+    if(currentTranslateX===-42){
+        sliderMainDiv.style.transform = `translate3d(0,0,0)`
+    }else{
+        sliderMainDiv.style.transform = `translate3d(${currentTranslateX-21}%,0,0)`
+    
+    }
     
 });
